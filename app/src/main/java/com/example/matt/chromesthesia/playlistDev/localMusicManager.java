04d08ea07772a3d0197715f3b49b2518b04b0bc5
iacaneda <iacaneda@Buffalo.edu>
2016-10-04 23:43:19 -0400

    created a class that will find local mp3s on the device's SD card

---
+++ app/src/main/java/com/example/matt/chromesthesia/playlistDev/localMusicManager.java
@@ -0,0 +1,13 @@
+package com.example.matt.chromesthesia.playlistDev;
+
+/**
+ * Created by Isabelle on 10/4/2016.
+ *
+ * This class reads the files from the SD card.
+ * Detects any files with .mp3 extensions
+ */
+
+
+
+public class localMusicManager {
+}
