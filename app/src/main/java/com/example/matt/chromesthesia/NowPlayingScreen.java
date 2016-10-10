d395227f018fe11b927d859e21456aa2ca9e9cdd
iacaneda <iacaneda@Buffalo.edu>
2016-10-09 22:52:01 -0400

    fixing library item picker

--- app/src/main/java/com/example/matt/chromesthesia/NowPlayingScreen.java
+++ app/src/main/java/com/example/matt/chromesthesia/NowPlayingScreen.java
@@ -1,17 +1,9 @@
 package com.example.matt.chromesthesia;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ImageButton;
-import android.widget.ToggleButton;
 import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.ToggleButton;
-
-import com.example.matt.chromesthesia.MPC;
 
 /**
  * Created by Matt on 10/8/2016.
@@ -25,8 +17,12 @@ public class NowPlayingScreen extends Chromesthesia {
 
         final ToggleButton playButton = (ToggleButton) findViewById(R.id.playButton);
 
-        //final MPC media = new MPC();
-        final MediaPlayer media = MediaPlayer.create(this, R.raw.sutphinboulevard);     //probably will be different when media reading is finished
+        /*final MPC media = new MPC();
+        iacaneda: If MediaPlayer.create needs a File to play, then we want our Library's
+        list options to change this MediaPlayer file.
+        */
+        final MediaPlayer media = MediaPlayer.create(this, R.raw.sutphinboulevard);
+
 
         playButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
