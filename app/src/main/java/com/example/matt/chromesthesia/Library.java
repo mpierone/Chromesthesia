8c182fd90bbd1f41d2dd0d1e5f9d7091b7dba3f7
iacaneda <iacaneda@Buffalo.edu>
2016-10-30 22:52:48 -0400

    Merged with Develop!

--- app/src/main/java/com/example/matt/chromesthesia/Library.java
+++ app/src/main/java/com/example/matt/chromesthesia/Library.java
@@ -1,17 +1,20 @@
 package com.example.matt.chromesthesia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.matt.chromesthesia.playlistDev.localMusicManager;
 
 import java.util.ArrayList;
-import java.util.Set;
 
 /**
  * Created by matt & will on 10/1/2016.
@@ -23,7 +26,8 @@ public class Library extends Chromesthesia {
     protected ArrayList<String> songArray;
     private ListView listView;
     private ListView songView;
-    public ArrayAdapter<String> arrayAdapter;
+    private ImageAdapter imgAdapter;
+    private ProgressBar progressB;
     localMusicManager lMM = new localMusicManager();
     public void onCreate(Bundle savedInstancedState) {
         super.onCreate(savedInstancedState);
@@ -36,7 +40,7 @@ public class Library extends Chromesthesia {
         }
         try
         {
-            arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songArray);
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, songArray);
             songView.setAdapter(arrayAdapter);
         }
         catch (NullPointerException e){
@@ -46,9 +50,77 @@ public class Library extends Chromesthesia {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 playSong(view, position);
-                positionVar = position;
+
+                if(mpservice.isPlaying())
+                {
+                    TextView songTitle = (TextView) findViewById(R.id.songTitleText);
+                    songTitle.setText(mpservice.getName());
+                    TextView artistName = (TextView) findViewById(R.id.artistText);
+                    artistName.setText(mpservice.getArtist());
+                }
             }
         });
+        final GridView gridview = (GridView) findViewById(R.id.libraryGridView);
+        gridview.setAdapter(imgAdapter = new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                if(position == 0){
+                    mpservice.playPrevious();
+                }
+                /*
+                if(mpservice.isPlaying()){
+                    imgAdapter.mThumbIds[1] = R.drawable.pausebutton;
+                }
+                else{
+                    imgAdapter.mThumbIds[1] = R.drawable.playbuttonunpressed;
+
+                }
+                */
+                if(position == 1){
+                    mpservice.resumePlay();
+                    //imgAdapter.mThumbIds[1] = R.drawable.playbuttonunpressed;
+                    //System.out.println(Integer.toString(imgAdapter.mThumbIds[1]));
+                }
+                if(position == 2){
+                    mpservice.pauseSong();
+                    //imgAdapter.mThumbIds[1] = R.drawable.pausebutton;
+                    //System.out.println(Integer.toString(imgAdapter.mThumbIds[1]));
+
+                }
+                if(position == 3){
+                    mpservice.playNext();
+                }
+                if(position == 4){
+                    Intent libraryToPlayScreenIntent = new Intent(v.getContext(), NowPlayingScreen.class);
+                    startActivity(libraryToPlayScreenIntent);
+                }
+            }
+        });
+
+        progressB = (ProgressBar) findViewById(R.id.progressB);
+        progressB.setMax(100);
+        Thread refresh = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                progressB.setProgress((int) (((float) mpservice.getPosition() / mpservice.getDuration()) * 100));
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        refresh.start();
+
+
     }
 
     public void createMusicList() {
